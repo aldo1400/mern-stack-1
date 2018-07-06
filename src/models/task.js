65d120b00facac31e6969f17e0e7f9cbc07778f1
Aldo Choque <achoque1400@gmail.com>
2018-07-06 08:38:42 -0500

    routes get,post,put,delete

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose');
 const {Schema}=mongoose;
 
-new Schema({
+const TaskSchema=new Schema({
     title:{type:String,required:true},
     description:{type:String,required:true}
-})
+});
+
+module.exports=mongoose.model('Task',TaskSchema);
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,11 +1,41 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/',(req,res)=>{
-    res.json({
-        status:'api works'
-    });
+const Task=require('../models/task');
+
+router.get('/',async(req,res)=>{
+    const Tasks=await Task.find();
+    res.json(Tasks);
 })
+
+router.get('/:id',async (req,res)=>{
+    const task=await Task.findById(req.params.id);
+    res.json(task);
+})
+
+router.post('/',async (req,res)=>{
+
+    const {title,description}=req.body;
+    const task=new Task({
+        title,description
+    });
+    await task.save();
+    console.log(task);
+    res.json({status:'Task saved'});
+});
+
+router.put('/:id',async(req,res)=>{
+    const {title,description}=req.body;
+    const newTask={title,description};
+    await Task.findOneAndUpdate(req.params.id,newTask);
+    res.json({status:'Task Updated'});
+
+});
+
+router.delete('/:id', async (req,res)=>{
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status:'Task eliminated'});
+});
 
 
 module.exports=router;
